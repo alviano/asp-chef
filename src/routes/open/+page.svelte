ef88c539fe2268aca158566abd5d1735772ad063
Mario Alviano <mario.alviano@gmail.com>
2023-05-26 20:15:20 +0200

    reboot

---
+++ src/routes/open/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {Recipe} from "$lib/recipe";
+    import {Utils} from "$lib/utils";
+
+    const [replacement, recipe] = location.hash.slice(1).split(';');
+    const replace = Utils.uncompress(replacement);
+    const data = Recipe.deserialize(recipe);
+    const new_recipe = Recipe.serialize(replace.input, data.encode_input, data.decode_output);
+    location = `/#${new_recipe}`;
+</script>
+
