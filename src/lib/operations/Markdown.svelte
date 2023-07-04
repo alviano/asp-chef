ffb7a4df321fe15a55a1c4827b36cc76a778d5fa
Mario Alviano <mario.alviano@gmail.com>
2023-07-04 20:04:04 +0200

    Markdown sorting

--- src/lib/operations/Markdown.svelte
+++ src/lib/operations/Markdown.svelte
@@ -24,6 +24,7 @@
     import {consts} from "$lib/consts";
     import {Base64} from "js-base64";
     import {Utils} from "$lib/utils";
+    import _ from "lodash";
 
 
     export let id;
@@ -50,107 +51,150 @@
         }
     }
 
+    function process_match(query_answer) {
+        if (query_answer.length === 0) {
+            throw Error("Expected one model, 0 found");
+        }
+
+        let matrix = null;
+        let separator = '\n';
+        let term_separator = ', ';
+        let prefix = '';
+        let suffix = '';
+        let sort = [];
+
+        const replacement = [];
+        let output_atoms = [];
+        Utils.parse_atoms(query_answer[0]).forEach(atom => {
+            if (atom.functor === undefined && atom.predicate === undefined) {
+                atom.functor = '';
+                atom.terms = [atom];
+            }
+            if (atom.functor === '') {
+                output_atoms.push(atom);
+            } else if (atom.predicate === 'matrix') {
+                output_atoms.push(atom);
+            } else if (atom.predicate === 'prefix') {
+                if (check_one_term_string(atom)) {
+                    prefix = atom.terms[0].string;
+                }
+            } else if (atom.predicate === 'suffix') {
+                if (check_one_term_string(atom)) {
+                    suffix = atom.terms[0].string;
+                }
+            } else if (atom.predicate === 'separator') {
+                if (check_one_term_string(atom)) {
+                    separator = atom.terms[0].string;
+                }
+            } else if (atom.predicate === 'term_separator') {
+                if (check_one_term_string(atom)) {
+                    term_separator = atom.terms[0].string;
+                }
+            } else if (atom.predicate === 'table') {
+                if (atom.terms.length !== 0) {
+                    Utils.snackbar(`Wrong number of terms in \#${index}. Markdown: ${atom.str}`);
+                } else {
+                    prefix = '|';
+                    term_separator = '|'
+                    suffix = '|';
+                    separator = '\n';
+                }
+            } else if (atom.predicate === 'list') {
+                if (atom.terms.length !== 0) {
+                    Utils.snackbar(`Wrong number of terms in \#${index}. Markdown: ${atom.str}`);
+                } else {
+                    prefix = '- ';
+                    term_separator = ', '
+                    suffix = '';
+                    separator = '\n';
+                }
+            } else if (atom.predicate === 'default') {
+                if (atom.terms.length !== 0) {
+                    Utils.snackbar(`Wrong number of terms in \#${index}. Markdown: ${atom.str}`);
+                } else {
+                    prefix = '';
+                    term_separator = ', '
+                    suffix = '';
+                    separator = '\n';
+                }
+            } else if (atom.predicate === 'sort') {
+                if (atom.terms.length === 0) {
+                    Utils.snackbar(`Wrong number of terms in \#${index}. Markdown: ${atom.str}`);
+                } else if (atom.terms.filter(term => term.number === undefined || term.number === 0).length > 0) {
+                    Utils.snackbar(`Wrong term in \#${index}. Markdown: ${atom.str}`);
+                } else {
+                    sort.push(atom.terms.map(term => term.number));
+                }
+            } else {
+                Utils.snackbar(`Unknown predicate in \#${index}. Markdown: ${atom.predicate}`);
+            }
+        });
+        sort.forEach(terms => {
+            const comparator = terms.map(sort_index => {
+                return atom => {
+                    if (atom.functor !== '') {
+                        return undefined;
+                    }
+                    const term = atom.terms[Math.abs(sort_index) - 1];
+                    if (term === undefined) {
+                        return undefined
+                    } else if (term.number !== undefined) {
+                        return term.number;
+                    } else {
+                        return term.str;
+                    }
+                }
+            });
+            output_atoms = _.orderBy(output_atoms, comparator, terms.map(sort_index => sort_index > 0 ? "asc" : "desc"));
+        });
+        output_atoms.forEach(atom => {
+            const terms = atom.terms.map(term => term.string || term.str);
+            if (atom.functor === '') {
+                replacement.push(prefix + terms.join(term_separator) + suffix);
+            } else if (atom.predicate === 'matrix') {
+                if (matrix === null) {
+                    matrix = [];
+                }
+                if (atom.terms.length < 3) {
+                    Utils.snackbar(`Wrong number of terms in \#${index}. Markdown: ${atom.str}`)
+                } else {
+                    const row = atom.terms[0].number;
+                    const col = atom.terms[1].number;
+                    const value = prefix + terms.slice(2).join(term_separator) + suffix;
+
+                    while (matrix.length < row) {
+                        matrix.push([]);
+                    }
+                    while (matrix[row-1].length < col) {
+                        matrix[row-1].push("");
+                    }
+
+                    matrix[row-1][col-1] = value;
+                }
+            }
+        });
+        if (matrix !== null) {
+            replacement.push(matrix.map(row => "|" + row.join("|") + "|").join("\n"));
+        }
+        return replacement.join(separator);
+    }
+
     onMount(() => {
         listeners.set(id, async (input) => {
             const the_output = [];
             for (let model of input) {
                 const output_part = [];
-                let separator = '\n';
-                let term_separator = ', ';
-                let prefix = '';
-                let suffix = '';
                 for (let atom of model.filter(atom => atom.predicate === options.predicate)) {
                     let md = Base64.decode(atom.terms[0].string);
-                    const matches = md.matchAll(/\{\{(((?!}}).)*)}}/gs);
+                    const matches = md.matchAll(/\{\{(=?)(((?!}}).)*)}}/gs);
                     if (matches !== null) {
                         for (let the_match of matches) {
-                            const match = the_match[1].trim();
-                            const program = model.map(atom => `${atom.str}.`).join('\n') + '\n#show.\n#show ' + match + (match.endsWith('.') ? '' : '.');
+                            const inline = the_match[1].trim();
+                            const match = the_match[2].trim();
+                            const program = model.map(atom => `${atom.str}.`).join('\n') + '\n#show.\n' +
+                                (inline ? '#show ' : '') + match + (match.endsWith('.') ? '' : '.');
                             const query_answer = await Utils.search_models(program, 1, true);
-                            let matrix = null;
-
-                            const replacement = [];
-                            if (query_answer.length === 1) {
-                                Utils.parse_atoms(query_answer[0]).forEach(atom => {
-                                    if (atom.functor === undefined && atom.predicate === undefined) {
-                                        atom.functor = '';
-                                        atom.terms = [atom];
-                                    }
-                                    const terms = atom.terms.map(term => term.string || term.str);
-                                    if (atom.functor === '') {
-                                        replacement.push(prefix + terms.join(term_separator) + suffix);
-                                    } else if (atom.predicate === 'matrix') {
-                                        if (matrix === null) {
-                                            matrix = [];
-                                        }
-                                        if (atom.terms.length < 3) {
-                                            Utils.snackbar(`Wrong number of terms in \#${index}. Markdown: ${atom.str}`)
-                                        } else {
-                                            const row = atom.terms[0].number;
-                                            const col = atom.terms[1].number;
-                                            const value = prefix + terms.slice(2).join(term_separator) + suffix;
-
-                                            while (matrix.length < row) {
-                                                matrix.push([]);
-                                            }
-                                            while (matrix[row-1].length < col) {
-                                                matrix[row-1].push("");
-                                            }
-
-                                            matrix[row-1][col-1] = value;
-                                        }
-                                    } else if (atom.predicate === 'prefix') {
-                                        if (check_one_term_string(atom)) {
-                                            prefix = atom.terms[0].string;
-                                        }
-                                    } else if (atom.predicate === 'suffix') {
-                                        if (check_one_term_string(atom)) {
-                                            suffix = atom.terms[0].string;
-                                        }
-                                    } else if (atom.predicate === 'separator') {
-                                        if (check_one_term_string(atom)) {
-                                            separator = atom.terms[0].string;
-                                        }
-                                    } else if (atom.predicate === 'term_separator') {
-                                        if (check_one_term_string(atom)) {
-                                            term_separator = atom.terms[0].string;
-                                        }
-                                    } else if (atom.predicate === 'table') {
-                                        if (atom.terms.length !== 0) {
-                                            Utils.snackbar(`Wrong number of terms in \#${index}. Markdown: ${atom.str}`);
-                                        } else {
-                                            prefix = '|';
-                                            term_separator = '|'
-                                            suffix = '|';
-                                            separator = '\n';
-                                        }
-                                    } else if (atom.predicate === 'list') {
-                                        if (atom.terms.length !== 0) {
-                                            Utils.snackbar(`Wrong number of terms in \#${index}. Markdown: ${atom.str}`);
-                                        } else {
-                                            prefix = '- ';
-                                            term_separator = ', '
-                                            suffix = '';
-                                            separator = '\n';
-                                        }
-                                    } else if (atom.predicate === 'default') {
-                                        if (atom.terms.length !== 0) {
-                                            Utils.snackbar(`Wrong number of terms in \#${index}. Markdown: ${atom.str}`);
-                                        } else {
-                                            prefix = '';
-                                            term_separator = ', '
-                                            suffix = '';
-                                            separator = '\n';
-                                        }
-                                    } else {
-                                        Utils.snackbar(`Unknown predicate in \#${index}. Markdown: ${atom.predicate}`);
-                                    }
-                                });
-                            }
-                            if (matrix !== null) {
-                                replacement.push(matrix.map(row => "|" + row.join("|") + "|").join("\n"));
-                            }
-                            md = md.replace(the_match[0], replacement.join(separator));
+                            md = md.replace(the_match[0], process_match(query_answer));
                         }
                     }
                     output_part.push(md);
@@ -173,16 +217,20 @@
         </p>
         <p>
             Models can be queried with the mustache syntax
-            <code>{`{{ (terms) : conjunctive_query }}`}</code>
+            <code>{`{{ program with #show directives }}`}</code>
+            (or <code>{`{{= (terms) : conjunctive_query }}`}</code> as a shortcut for <code>{`{{ #show (terms) : conjunctive_query. }}`}</code>).
+        </p>
+        <p>
+            Output can be ordered via the varadics predicate <code>sort</code>, specifying the indices of the terms to use (positive for ascending, negative for descending).
         </p>
         <p>
             The separator of obtained substitutions can be specified with
             <code>{`{{ separator("\n") }}`}</code>.
             Similarly, <code>term_separator/1</code>, <code>prefix/1</code> and <code>suffix/1</code> can be used to customize the print of each obtained substitution.
-            Finally, <code>table/0</code>, <code>list/0</code> and <code>default/0</code> can be used to set the above four parameters to accommodate specific common output formats.
+            Moreover, <code>table/0</code>, <code>list/0</code> and <code>default/0</code> can be used to set the above four parameters to accommodate specific common output formats.
         </p>
         <p>
-
+            Additionally, <code>matrix/3</code> can be used to produce a table by specifying values for each cell.
         </p>
         <p>
             The input is echoed in output.
