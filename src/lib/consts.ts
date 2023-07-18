c613b6abf16968c37596d82f5142e24d598dda5e
Mario Alviano <mario.alviano@gmail.com>
2023-07-18 11:52:05 +0200

    allow HTTP links in markdown

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -3,8 +3,7 @@ export const consts = {
   DOMAIN: import.meta.env.VITE_DOMAIN,
   PURIFY_LOG_ERRORS: import.meta.env.VITE_PURIFY_LOG_ERRORS === 'true',
   PURIFY_EXTERNAL_LINK_PREFIXES: [
-    'https://ssd2020.pythonanywhere.com/',
-    'https://cod.alviano.org/',
+    'http',  // allow any HTTP link
   ],
   PURIFY_ALLOWED_TAGS: 'a blockquote body br code em h1 h2 h3 h4 h5 h6 hr img li ol p pre span strong table tbody td th thead tr ul #text'.split(' '),
   PURIFY_ALLOWED_ATTR: 'alt class href src style title'.split(' '),
