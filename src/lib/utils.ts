85069045f109690cb27780d26f110fbb22b2ad3c
Mario Alviano <mario.alviano@gmail.com>
2023-07-18 11:33:00 +0200

    fix bug in producing the costs atom for optimize

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -119,11 +119,11 @@ export class Utils extends BaseUtils {
                 }
             }
             if (cost_predicate) {
-                if (result.Models.Costs) {
-                    res.forEach(model => model.push(`${cost_predicate}((${result.Models.Costs.join(',')},))`));
-                } else {
-                    res.forEach(model => model.push(`${cost_predicate}((,))`));
-                }
+                const costs = result.Models.Costs ? result.Models.Costs.join(',') : "";
+                const end = result.Models.Costs.length <= 1 ? "," : "";
+                const cost_atom = `${cost_predicate}((${costs}${end}))`;
+                res.forEach(model => model.push(cost_atom));
+                console.log(cost_atom)
             }
             return res;
         }
