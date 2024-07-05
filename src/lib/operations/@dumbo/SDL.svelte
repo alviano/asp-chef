477cd7e8f87cd009ddef909739291e5af3778b84
Mario Alviano <mario.alviano@gmail.com>
2024-07-05 18:10:46 +0200

    add SDL

---
+++ src/lib/operations/@dumbo/SDL.svelte
@@ -0,0 +1,66 @@
+<script context="module">
+    import {Recipe} from "$lib/recipe";
+    import {Dumbo} from "$lib/operations/@dumbo/dumbo";
+    import {Base64} from "js-base64";
+
+    const operation = "@dumbo/SDL";
+    const default_extra_options = {
+        program_predicate: '__program__',
+    };
+
+    Recipe.register_operation_type(operation, async (input, options, index) => {
+        const res = [];
+        for (const part of input) {
+            try {
+                const input_part = [];
+                const program = [];
+                part.forEach(atom => {
+                    if (atom.predicate === options.program_predicate) {
+                        program.push(Base64.decode(atom.terms[0].string));
+                        return;
+                    }
+                    input_part.push(atom);
+                });
+                const json = await Dumbo.fetch("sdl", {
+                    program: program.join('\n'),
+                });
+                input_part.push(Dumbo.encode_program(json.program, options.program_predicate));
+                res.push(input_part);
+            } catch (error) {
+                Recipe.set_errors_at_index(index, error, res);
+            }
+        }
+        return res;
+    });
+</script>
+
+<script>
+    import Operation from "$lib/Operation.svelte";
+    import {Input, InputGroup, InputGroupText} from "sveltestrap";
+
+    export let id;
+    export let options;
+    export let index;
+    export let add_to_recipe;
+    export let keybinding;
+
+    function edit() {
+        Recipe.edit_operation(id, index, options);
+    }
+</script>
+
+<Operation {id} {operation} {options} {index} {default_extra_options} {add_to_recipe} {keybinding}>
+    <div slot="description">
+        <p>
+            The <strong>{operation}</strong> operation rewrites the Structured Declarative Language (SDL) program stored in <code>__program__</code> into an equivalent ASP version.
+        </p>
+    </div>
+    <InputGroup>
+        <InputGroupText style="width: 8em;">Program</InputGroupText>
+        <Input type="text"
+               bind:value={options.program_predicate}
+               placeholder="program predicate"
+               on:input={edit}
+        />
+    </InputGroup>
+</Operation>
