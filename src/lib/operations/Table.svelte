d873f43ffdb93783049b4427eab0b7844412a884
malvi <mario.alviano@gmail.com>
2024-12-12 10:40:48 +0100

    add options to hide UI elements from Table

--- src/lib/operations/Table.svelte
+++ src/lib/operations/Table.svelte
@@ -4,6 +4,9 @@
     const operation = "Table";
     const default_extra_options = {
         search: '',
+        hide_search: false,
+        hide_model_index: false,
+        hide_sort_buttons: false,
     };
 
     const listeners = new Map();
@@ -67,47 +70,59 @@
 </script>
 
 <Operation {id} {operation} {options} {index} {default_extra_options} {add_to_recipe} {keybinding}>
+    <InputGroup>
+        <InputGroupText>UI Elements</InputGroupText>
+        <Button outline="{options.hide_search}" on:click={() => { options.hide_search = !options.hide_search; edit(); }}>Search</Button>
+        <Button outline="{options.hide_model_index}" on:click={() => { options.hide_model_index = !options.hide_model_index; edit(); }}>Model Index</Button>
+        <Button outline="{options.hide_sort_buttons}" on:click={() => { options.hide_sort_buttons = !options.hide_sort_buttons; edit(); }}>Sort Buttons</Button>
+    </InputGroup>
     <div slot="output">
-        <InputGroup>
-            <InputGroupText>Search</InputGroupText>
-            <Input type="search" placeholder="Search..." bind:value={options.search} on:input={edit} data-testid="Table-search" />
-        </InputGroup>
+        {#if !options.hide_search}
+            <InputGroup>
+                <InputGroupText>Search</InputGroupText>
+                <Input type="search" placeholder="Search..." bind:value={options.search} on:input={edit} data-testid="Table-search" />
+            </InputGroup>
+        {/if}
         <div class="m-1" style="overflow-y: auto;">
             {#each models as model, model_index}
-                <h6 class="text-center">Model #{model_index + 1}</h6>
+                {#if !options.hide_model_index}
+                    <h6 class="text-center">Model #{model_index + 1}</h6>
+                {/if}
                 <Table bordered data-testid="Table">
-                    <thead>
-                        <tr>
-                            <th>
-                                <Popover block title="Sort by predicate" value="Add a Sort operation before this ingredient.">
-                                    <ButtonGroup class="align-content-center">
-                                    <InputGroupText>Predicate</InputGroupText>
-                                            <Button size="sm" on:click={() => add_sort(0, true)} data-testid="Table-sort-desc-0">
-                                                <Icon name="arrow-up" />
-                                            </Button>
-                                        <Button size="sm" on:click={() => add_sort(0, false)} data-testid="Table-sort-asc-0">
-                                            <Icon name="arrow-down" />
-                                        </Button>
-                                    </ButtonGroup>
-                                </Popover>
-                            </th>
-                            {#each argument_indices(model) as arg_index}
+                    {#if !options.hide_sort_buttons}
+                        <thead>
+                            <tr>
                                 <th>
-                                    <Popover block title="Sort by argument #{arg_index + 1}" value="Add a Sort operation before this ingredient.">
+                                    <Popover block title="Sort by predicate" value="Add a Sort operation before this ingredient.">
                                         <ButtonGroup class="align-content-center">
-                                            <InputGroupText>Arg#{arg_index + 1}</InputGroupText>
-                                            <Button size="sm" on:click={() => add_sort(arg_index + 1, true)} data-testid="Table-sort-desc-{arg_index + 1}">
-                                                <Icon name="arrow-up" />
-                                            </Button>
-                                            <Button size="sm" on:click={() => add_sort(arg_index + 1, false)} data-testid="Table-sort-asc-{arg_index + 1}">
+                                        <InputGroupText>Predicate</InputGroupText>
+                                                <Button size="sm" on:click={() => add_sort(0, true)} data-testid="Table-sort-desc-0">
+                                                    <Icon name="arrow-up" />
+                                                </Button>
+                                            <Button size="sm" on:click={() => add_sort(0, false)} data-testid="Table-sort-asc-0">
                                                 <Icon name="arrow-down" />
                                             </Button>
                                         </ButtonGroup>
                                     </Popover>
                                 </th>
-                            {/each}
-                        </tr>
-                    </thead>
+                                {#each argument_indices(model) as arg_index}
+                                    <th>
+                                        <Popover block title="Sort by argument #{arg_index + 1}" value="Add a Sort operation before this ingredient.">
+                                            <ButtonGroup class="align-content-center">
+                                                <InputGroupText>Arg#{arg_index + 1}</InputGroupText>
+                                                <Button size="sm" on:click={() => add_sort(arg_index + 1, true)} data-testid="Table-sort-desc-{arg_index + 1}">
+                                                    <Icon name="arrow-up" />
+                                                </Button>
+                                                <Button size="sm" on:click={() => add_sort(arg_index + 1, false)} data-testid="Table-sort-asc-{arg_index + 1}">
+                                                    <Icon name="arrow-down" />
+                                                </Button>
+                                            </ButtonGroup>
+                                        </Popover>
+                                    </th>
+                                {/each}
+                            </tr>
+                        </thead>
+                    {/if}
                     <tbody>
                         {#if model.length === 0}
                             <tr>
