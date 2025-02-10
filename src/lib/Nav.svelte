5067985c29ea2bddedf6fe1cb599310bc83c77a3
malvi <mario.alviano@gmail.com>
2025-02-10 21:01:12 +0100

    fix hidden ? button (and simplify Nav)

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,12 +1,6 @@
 <script>
   import {Collapse, Icon, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from '@sveltestrap/sveltestrap';
   import Popover from "$lib/Popover.svelte";
-
-  let isOpen = false;
-
-  function handleUpdate(event) {
-      isOpen = event.detail.isOpen;
-  }
 </script>
 
 <Navbar color="dark" expand="md" class="navbar-dark d-print-none" dark>
@@ -15,30 +9,27 @@
       ASP Chef
     </span>
   </NavbarBrand>
-  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-    <Nav class="ms-auto" navbar>
-      <Popover title="About">
-        <div slot="value">
-          <p>
-            ASP Chef is a simple, intuitive web app for analysing answer sets without having to deal with complex tools or programming languages.
-          </p>
-          <p>
-            Proudly brought to you by Mario Alviano, Davide Cirimele and Luis Angel Rodriguez Reiners.
-          </p>
-          <p>
-            More details on the GitHub repository <code>https://github.com/alviano/asp-chef</code>.
-          </p>
-          <p>
-            <strong>Click for some examples and tutorials!</strong>
-          </p>
-        </div>
-        <NavItem>
-          <NavLink href="/s" target="_blank"><Icon name="question-circle" /></NavLink>
-        </NavItem>
-      </Popover>
-    </Nav>
-  </Collapse>
+  <Nav class="ms-auto" navbar>
+    <Popover title="About">
+      <div slot="value">
+        <p>
+          ASP Chef is a simple, intuitive web app for analysing answer sets without having to deal with complex tools or programming languages.
+        </p>
+        <p>
+          Proudly brought to you by Mario Alviano, Davide Cirimele and Luis Angel Rodriguez Reiners.
+        </p>
+        <p>
+          More details on the GitHub repository <code>https://github.com/alviano/asp-chef</code>.
+        </p>
+        <p>
+          <strong>Click for some examples and tutorials!</strong>
+        </p>
+      </div>
+      <NavItem>
+        <NavLink href="/s" target="_blank"><Icon name="question-circle" /></NavLink>
+      </NavItem>
+    </Popover>
+  </Nav>
 </Navbar>
 
 <style>
