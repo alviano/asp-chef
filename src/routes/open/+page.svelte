ed234784ecfb4be57ae44b3332c2b865f5a33b22
malvi <mario.alviano@gmail.com>
2025-10-14 08:04:36 +0200

    support empty parts in /open endpoint

--- src/routes/open/+page.svelte
+++ src/routes/open/+page.svelte
@@ -2,9 +2,10 @@
     import {Recipe} from "$lib/recipe";
     import {Utils} from "$lib/utils";
 
-    const [replacement, recipe] = location.hash.slice(1).split(';');
-    const replace = Utils.uncompress(replacement);
-    const data = Recipe.deserialize(recipe);
+    const hash = location.hash || ";";
+    const [replacement, recipe] = hash.slice(1).split(';');
+    const replace = replacement ? Utils.uncompress(replacement) : { input: "" };
+    const data = recipe ? Recipe.deserialize(recipe) : { recipe: [] };
     delete data.input;
     const new_recipe = Recipe.serialize(replace.input, {...data});
     location = `/#${new_recipe}`;
