7507638aa939e64c4c49347569eb661736b495fe
malvi <mario.alviano@gmail.com>
2026-05-19 08:58:55 +0200

    increase memory for building on netlify

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [
-        sveltekit(),
-    ],
+    plugins: [sveltekit()],
+    optimizeDeps: {
+        exclude: ['swipl-wasm']
+    },
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}'],
         environment: 'jsdom',
         globals: true,
-        setupFiles: ["@testing-library/jest-dom/extend-expect"]
-    },
+        setupFiles: ['@testing-library/jest-dom/extend-expect']
+    }
 };
 
 export default config;
