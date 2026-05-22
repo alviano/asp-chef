79f84dab8aa553757a0f2e9151ce82940c9b29d0
malvi <mario.alviano@gmail.com>
2026-05-22 22:34:49 +0200

    increase memory for building on netlify (bypass it on local dev)

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 /** @type {import('vite').UserConfig} */
-const config = {
-    plugins: [sveltekit()],
-    optimizeDeps: {
-        exclude: ['swipl-wasm']
-    },
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}'],
-        environment: 'jsdom',
-        globals: true,
-        setupFiles: ['@testing-library/jest-dom/extend-expect']
-    }
-};
+const config = defineConfig(({ command, mode }) => {
+    const isProduction = command === 'build';
+
+    return {
+        plugins: [sveltekit()],
+        optimizeDeps: {
+            exclude: isProduction ? ['swipl-wasm'] : [],
+        },
+        test: {
+            include: ['src/**/*.{test,spec}.{js,ts}'],
+            environment: 'jsdom',
+            globals: true,
+            setupFiles: ['@testing-library/jest-dom/extend-expect']
+        }
+    };
+});
+
 
 export default config;
